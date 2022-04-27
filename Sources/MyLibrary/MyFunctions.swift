4aed979026fab9f4f8af1f9672f156472c27a794
amit1650 <amit1@appinventiv.com>
2022-04-27 17:37:16 +0530

    Updated code

--- Sources/MyLibrary/MyFunctions.swift
+++ Sources/MyLibrary/MyFunctions.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+
+public class MyFunctions{
+    public init () {
+        
+    }
+    
+    public func printValue(_ val: String) -> String{
+        return val
+    }
+}
